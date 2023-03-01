3eb7da5b38725a15ba3de401f96adc090a29fdc4
ahsanhd <112783139+ahsanhd@users.noreply.github.com>
2023-03-02 01:55:01 +0500

    Basic Js AS
    delete falsey values from the array and use a new string to not change the original array

---
+++ Basic Algorithm Scripting/falseyBouncer.js
@@ -0,0 +1,34 @@
+/*
+
+Remove all falsy values from an array. Return a new array; do not mutate the original array.
+
+Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
+
+Hint: Try converting each value to a Boolean.
+
+*/
+
+function bouncer(arr) {
+    let arrCopy = arr.slice();
+    let bool = true;
+    let arrl = [];
+    for(let i = 0; i < arrCopy.length; i++){
+      bool = Boolean(arrCopy[i]);
+      if(bool == false){
+       delete arrCopy[i];
+  
+      }
+     // console.log(arrCopy[i])
+      if(arrCopy[i]){                       // does this return to true idk Ask::: Hamza about this
+        arrl.push(arrCopy[i])
+      }
+    }
+  
+    return arrl;
+   
+  }
+  
+  //bouncer([7, "ate", "", false, 9]);
+  
+  
+  console.log(bouncer([null, NaN, 1, 2, undefined]))
